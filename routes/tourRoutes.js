b98be990a0244fbcad31caf0674690b5e62d29f2
anns318 <an310898@gmai.com>
2023-07-25 22:42:19 +0700

    add mongoose and refactor folder

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const tourController = require("../controller/tourController");
+const tourController = require("../controllers/tourController");
 
 const router = express.Router();
 
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
-//* Start server
+
+const db = process.env.DATABASE_CONNECT_STRING.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+  })
+  .then((connect) => {
+    console.log(connect.connections);
+  });
+
 const port = process.env.PORT;
 app.listen(port);
