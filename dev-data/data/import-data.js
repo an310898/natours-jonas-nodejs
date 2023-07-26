9fdd42c5d3278b326d47c6dfb4f6604567dcd55d
anns318 <an310898@gmai.com>
2023-07-26 15:49:30 +0700

    import data with run script practice

---
+++ dev-data/data/import-data.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const { Tour } = require("../../models/tour");
+
+dotenv.config({ path: "./config.env" });
+
+const connectString = process.env.DATABASE_CONNECT_STRING.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(connectString, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("connect db success!");
+  });
+
+const importData = async () => {
+  await Tour.deleteMany({});
+  fs.readFile(`${__dirname}/tours-simple.json`, async (err, content) => {
+    if (!err) {
+      await Tour.create(JSON.parse(content));
+      console.log("import success!");
+      process.exit();
+    }
+  });
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+}
+
+//* run this comment to import
+//* node dev-data/data/import-data --import
